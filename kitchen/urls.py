3871e6a538a6dd9cf9c5a21c104951cc14eba1cb
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-01-28 20:51:46 +0200

    Create forms, views, and templates for managing dishes

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from . import views
+from .views import(
+    index,
+    DishListView,
+    DishDetailView,
+    DishCreateView,
+    DishUpdateView,
+    DishDeleteView,
+)
 
 app_name = "kitchen"
 
 urlpatterns = [
-    path("", views.index, name="index"),  # Головна сторінка
+    path("", index, name="index"),  # Головна сторінка
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
+    path("dishes/<int:pk>/update/", DishUpdateView.as_view(), name="dish-update"),
+    path("dishes/<int:pk>/delete/", DishDeleteView.as_view(), name="dish-delete"),
 
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Cook, DishType, Dish
+from .forms import DishSearchForm,\
+    DishForm
 
 
 def index(request):
@@ -25,3 +27,47 @@ def index(request):
     }
 
     return render(request, "kitchen/index.html", context=context)
+
+
+class DishListView(LoginRequiredMixin, generic.ListView):
+    model = Dish
+    context_object_name = "dish_list"
+    template_name = "kitchen/dish_list.html"
+    paginate_by = 5
+    queryset = Dish.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DishListView, self).get_context_data(**kwargs)
+        context["search_form"] = DishSearchForm
+        return context
+
+    def get_queryset(self):
+        form = DishSearchForm(self.request.GET)
+        if form.is_valid():
+            return self.queryset.filter(
+                name__icontains=form.cleaned_data["name"])
+        return self.queryset
+
+
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Dish
+
+
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    form_class = DishForm
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Dish
+    form_class = DishForm
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Dish
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+
