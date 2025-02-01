c2f1547161c7c3b9868923ec7f1630cf3fe81c69
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-02-01 18:58:00 +0200

    Create Ingredient model and add ManyToManyField 'ingredients' to the Dish model to associate ingredients with dishes.

--- kitchen/forms.py
+++ kitchen/forms.py
@@ -64,3 +64,4 @@ class DishTypeSearchForm(forms.Form):
         )
     )
 
+
--- kitchen/models.py
+++ kitchen/models.py
@@ -27,12 +27,22 @@ class Cook(AbstractUser):
         return reverse("kitchen:cook-detail", kwargs={"pk": self.pk})
 
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    quantity = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
     cooks = models.ManyToManyField(Cook, related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes", blank=True)
 
     class Meta:
         ordering = ["name"]
