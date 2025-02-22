79099ecfc344dde9120f687889d079a728893622
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-02-22 20:08:51 +0200

    Refactor index and toggle_dish_assign from function-based views to class-based views; update corresponding URLs

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import TemplateView
