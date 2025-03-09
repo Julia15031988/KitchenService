95b597d0e42d467411cac229fdabf0be6ee25441
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-03-09 13:20:14 +0200

    update settings

--- KitchenService/settings.py
+++ KitchenService/settings.py
@@ -22,10 +22,10 @@ load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = "True"
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
