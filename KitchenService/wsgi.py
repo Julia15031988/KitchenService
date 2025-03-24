67fd2de4c2858b0c2fc79a4c03469aeca348c753
Julia Okhrymenko <Ohrimenko15@gmail.com>
2025-03-24 20:14:50 +0200

    change wsgi

--- KitchenService/wsgi.py
+++ KitchenService/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KitchenService.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "KitchenService.config.settings")
 application = get_wsgi_application()
 
